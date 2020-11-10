59ae1af8cedbd51f2fcae2f4739f08f11d8798bd
somundane <98dana@gmail.com>
2020-11-09 22:12:51 -0500

    Random testing -- not working; will revert
    pushing to save

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 let vid;
 
 let stars, swirl, astro, tv, upside, walk, type, paper, bird, water, house, kite;
-let yell, blu;
 function preload() {
     //images from unsplash; full list @ sources.txt
     stars = loadImage('images/1.png');
@@ -20,57 +19,141 @@ function preload() {
 }
 
 let b = 0;
+let timer;
+
+let yell, blu;
+let yell2, blu2;
+let star2;
 function setup() {
+    timer = millis();
     createCanvas(1000, 1000);
     pixelDensity(1);
     
     yell = createImage(width, height)
     blu = createImage(width, height);
+    yell2 = createImage(width, height)
+    blu2 = createImage(width, height);
+    star2 = createImage(width, height);
     
     motion(paper);
-    stamp(walk, yell, blu);
+        stamp(walk, yell, blu);
+        stamp(kite, yell2, blu2);
+    motion(house);
+    motion(swirl);
 
+    image(stars, 0, 0);
+        block(swirl);
+    image(swirl, 0, 0);
+    image(astro, 0, 0);
+    image(tv, 0, 0);
+    image(upside, 0, 0);
+    image(walk, 0, 0);
+    image(type, 0, 0);
+    image(paper, 0, 0);
+    image(water, 0, 0);
+    image(house, 0, 0);
+    image(kite, 0, 0);
+    image(yell, 10, 0);
+    image(blu, -10, 0);
+    image(yell2, 10, 0);
+    image(blu2, -10, 0);
+    
     vid = createCapture(VIDEO);
     vid.size(320, 240);
     vid.hide();
     
 }
 
+let birdpos = [];
+let store = false;
+let hoverbird;
 function draw() {
-    background(0);
-    b = map(mouseX, 0, width, 0, 60);
-    
-    //images
-    image(stars, 0, 0);
-        wnoise(stars);
-    image(swirl, 0, 0);
-        block(swirl);
-        wnoise(swirl);
-    image(astro, 0, 0);
+    wnoise(stars);
+    image(star2, 0, 0);
+    //background(0);
     image(vid, 430, 710, 320 * 0.50, 240 * 0.50);
+        screen(vid, 25);
     image(tv, 0, 0);
+    
     image(upside, 0, 0);
     image(walk, 0, 0);
     image(type, 0, 0);
     image(paper, 0, 0);
     image(bird, 0, 0);
-    //if ___ rainbow down
-    //else rainbow up (aurora borealis style)
         rainbow(bird);
     image(water, 0, 0);
     image(house, 0, 0);
-        wnoise(house);
     image(kite, 0, 0);
-    
-    image(yell, 10, 0);
-    image(blu, -10, 0);
+//    
+//    image(yell, 10, 0);
+//    image(blu, -10, 0);
+//    pixelate();
     
 }
 
-let starsloc = [];
-function wnoise(img) {
+function pixelate() {
+    
+}
+
+function mousePressed () {
+    loadPixels();
+    
+   for (var y = 0; y < img.height; y++) {
+            for (var x = 0; x < img.width; x++) {
+                var index = (x + y * img.width)*4;
+        
+                let r = img.pixels[index+0];
+                let g = img.pixels[index+1];
+                let b = img.pixels[index+2]; 
+                let a = img.pixels[index+3];  
+            
+                let avg = (r + g + b) / 3;
+                img.pixels[index+0] = avg;
+                img.pixels[index+1] = avg;
+                img.pixels[index+2] = avg;
+                
+
+        }
+      }
+  
+    updatePixels();
+}
+
+function screen(img, shade) {
     img.loadPixels();
         for (var y = 0; y < img.height; y++) {
+            for (var x = 0; x < img.width; x++) {
+                var index = (x + y * img.width)*4;
+        
+                let r = img.pixels[index+0];
+                let g = img.pixels[index+1];
+                let b = img.pixels[index+2]; 
+                let a = img.pixels[index+3];  
+            
+                let avg = (r + g + b) / 3;
+                
+                if(avg > 150) {
+                    img.pixels[index+3] = 100;
+                }
+                else {
+                    img.pixels[index+2] += 50;
+                } 
+                if(avg < shade) {
+                    img.pixels[index+0] += 50;
+                }
+
+//                img.pixels[index+0] = avg + 30;
+//                img.pixels[index+1] = avg + 30;
+//                img.pixels[index+2] = avg + 30;
+        }
+      }
+      img.updatePixels(); 
+}
+let starsloc = [];
+function wnoise(img) {
+    star2.loadPixels();
+    img.loadPixels();
+        for (var y = 0; y < img.height/2; y++) {
             for (var x = 0; x < img.width; x++) {
                 var index = (x + y * img.width)*4;
             
@@ -81,16 +164,28 @@ function wnoise(img) {
 
                 if(r > 100 && g > 100 && b > 100) {
                     let r = random(120, 255);
-                    img.pixels[index+0] = r-5;
-                    img.pixels[index+1] = r-5;
-                    img.pixels[index+2] = r;
-                    //stars.pixels[index+3] = 200; 
+                    r = r-5;
+                    g = r-5;
+                    b = r;
+                } 
+                else {
+                    a = 0;
                 }
-            
-          
+                
+                let c = color(r,g,b,a);
+                star2.set(x, y, c);
+                
+//                if(r > 100 && g > 100 && b > 100) {
+//                    let r = random(120, 255);
+//                    img.pixels[index+0] = r-5;
+//                    img.pixels[index+1] = r-5;
+//                    img.pixels[index+2] = r;
+//                } 
+                
         }
       }
-      img.updatePixels();
+    star2.updatePixels();
+    img.updatePixels();
 }
 
 //mask
@@ -107,12 +202,7 @@ function block(img) {
             
                 if(r < 50 && g < 50 && b < 50) {
                     img.pixels[index+3] = 100;
-                    //stars.pixels[index+3] = 200; 
                 }
-          
-          //let avg = (r + g + b) / 3;
-          
-          
         }
       }
       img.updatePixels();
@@ -161,9 +251,9 @@ function motionBlur(img, x, y) {
     img.pixels[trueIndex + 1] = avgG;
     img.pixels[trueIndex + 2] = avgB;
 }
-function stamp(img, yell, blu) {
-    yell.loadPixels();
-    blu.loadPixels();
+function stamp(img, one, two) {
+    one.loadPixels();
+    two.loadPixels();
     img.loadPixels();
         for (var y = 0; y < img.height; y++) {
             for (var x = 0; x < img.width; x++) {
@@ -183,130 +273,89 @@ function stamp(img, yell, blu) {
                     a = a * 0.5;
                 }
                 let c = color(r,g,b,a);
-                yell.set(x, y, c);
+                one.set(x, y, c);
                 
                 r = r-255;
                 g = g-255;
                 r = r/2;
                 g = g/2;
                 c = color(r,g,b,a);
-                blu.set(x, y, c);
+                two.set(x, y, c);
         }
       }
-    yell.updatePixels();
-    blu.updatePixels();
+    one.updatePixels();
+    two.updatePixels();
 }
-let changed = false;
-let max = false;
-let ogr;
-let originals = [];
-let got = false;
-let adding = true;
-let timer;
-let initial = true;
+
 function rainbow(img) {
+    
     img.loadPixels();
         for (var y = 0; y < img.height; y++) {
             for (var x = 0; x < img.width; x++) {
                 var index = (x + y * img.width)*4;
-                
+                var mouse = (mouseX + mouseY * img.width)*4
+
                 let r = img.pixels[index+0];
                 let g = img.pixels[index+1];
                 let b = img.pixels[index+2];
-                let a = img.pixels[index+3]; 
-                
-                
-//                if(got == false) {
-//                    let red = {
-//                        val: r,
-//                        state: 0
-//                    }
-//                    originals.push(red);
-//                    originals.push(g);
-//                    originals.push(b);
-//                    originals.push(a);
-//                }
+                let a = img.pixels[index+3];
+
                 if(r > 70 && r < 200 && g < 140 && b < 140)
                     img.pixels[index+0] += 5;
                 if(b > 70 && b < 200 && r < 140 && g < 140)
                     img.pixels[index+2] += 5;
-
-//                
-//                if(r < 10)
-//                    adding == true
-//                
-//                
-//                if(adding == true)
-//                    img.pixels[index+0] += 5;
-//                if(adding == false)
-//                    img.pixels[index+0] -=5;
                 
-//                
-//                var max = (img.width + img.height * img.width)*4;
-
-//                
-//                if(r == 255) {
-//                    originals[index].state = 1;
-//                }
-//                if(originals[index].state == 0;){
-//                   img.pixels[index+0] ++; 
-//                }
-//                
-//                //this means it has reached max value
-//                if(originals[index].state == 1) {
-//                    if(r > originals[index].r)
-//                        img.pixels[index+0] --;
-//                    if(r == originals[index].r)
-//                        originals[index].state = 0;
-//                }
-//                if(r<255 && changed == false){
-//                    img.pixels[index+0] ++;
-//                    if (r >250 && changed == false) {
-//                        changed == true;
-//                        print('thisruns');
-//                    }
-//                }
-//
-//                
-//                if(r > originals[index] && changed == true) {
-//                     img.pixels[index+0]-= 10;
-//                    if(r < originals[index] + 5 && changed == true)
-//                        changed == false;
+//                if(index == mouse && a > 230) {
+//                    print('tru');
+//                    fadeBird(img);
+//                    //hoverbird == true
 //                    
 //                }
-                   
-
-//                
-//                if(r > 150 && b < 100 && g < 100 && changed == false) {
-//                        img.pixels[index+0]-=1;
-//                    //done changing
-//                    if(r < 160)
-//                        changed = true;
+//                else {
+//                    colorBird(img);
 //                }
-//                
-//                if(changed == true && r < originals[index]) {
-//                    img.pixels[index+0]+=1;
-//                    if(r > originals[index] - 10) 
-//                        changed == false;
-//                }
-//                
-//                if(r > 145 && r <= 150) {
-//                    img.pixels[index+3] == 250;
-//                }
-//                if(a == 250 && r < 235) {
-//                    img.pixels[index+0]+=5;
-//                }
-//                if(r > 225 && r <= 235) {
-//                    img.pixels[index+3] == 255;
-//                }
-//                if(b > 150) {
-//                    img.pixels[index+0]+=5;
-//                }
-        }
+            }
       }
-    if(got == false)
-        print('ends push')
-    got = true;
-
-      img.updatePixels();
+    img.updatePixels();
 }
+
+//function colorBird(img) {
+//     img.loadPixels();
+//        for (var y = 0; y < img.height; y++) {
+//            for (var x = 0; x < img.width; x++) {
+//                var index = (x + y * img.width)*4;
+//
+//                let r = img.pixels[index+0];
+//                let g = img.pixels[index+1];
+//                let b = img.pixels[index+2];
+//                let a = img.pixels[index+3];
+//
+//                if(r > 70 && r < 200 && g < 140 && b < 140)
+//                    img.pixels[index+0] += 5;
+//                if(b > 70 && b < 200 && r < 140 && g < 140)
+//                    img.pixels[index+2] += 5;
+//            }
+//        }
+//
+//    img.updatePixels();
+//}
+//function fadeBird(img) {
+//     img.loadPixels();
+//        for (var y = 0; y < img.height; y++) {
+//            for (var x = 0; x < img.width; x++) {
+//                var index = (x + y * img.width)*4;
+//
+//                let r = img.pixels[index+0];
+//                let g = img.pixels[index+1];
+//                let b = img.pixels[index+2];
+//                let a = img.pixels[index+3];
+//
+//                if(r > 70 && r < 200 && g < 140 && b < 140)
+//                    img.pixels[index+0] -= 5;
+//                if(b > 70 && b < 200 && r < 140 && g < 140)
+//                    img.pixels[index+2] -= 5;
+//            }
+//        }
+//
+//    img.updatePixels();
+//}
